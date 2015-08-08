ff82e2c7b5e489fcfb393bcb10d062311183bfab
Prelang Builder <builder@prelang.com>
2015-08-08 23:59:13 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Investor' to allow for create, update

--- app/admin/investor.rb
+++ app/admin/investor.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Investor do
+  permit_params :title, :linked_in, :fax, :price_from, :price_to, :verified, :principal, :subscribed, :phone, :phone_2
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
