e742fee412a68927b77874a1df047170cc3683fd
Prelang Builder <builder@prelang.com>
2015-08-08 23:59:03 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Asset' to allow for create, update

--- app/admin/asset.rb
+++ app/admin/asset.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Asset do
+  permit_params :name, :description, :size, :land_size, :price, :price_psf, :hide_price, :cap_rate, :occupancy, :highlights, :year_built
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
